5730c180f128d7ffc476f5b8324a14c1afc67771
patrick-atienza25 <patienza25@gmail.com>
2022-11-17 13:02:21 -0800

    progress

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,53 +8,79 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
-
+            if(char.IsLetter(c))
+            {
+                return true;
+            }
+            else
+                return false;
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            return vals.Length % 2 == 0;
         }
 
         public bool IsNumberEven(int number)
         {
-            return number % 2 == 0;
+            if (number % 2 == 0)
+                return true;
+            else
+                return false;
         }
 
-        public bool IsNumberOdd(int num)
-        {
-            throw new NotImplementedException();
-        }
+        public bool IsNumberOdd(int num) => (num % 2 != 0);
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+            return numbers.Min() + numbers.Max();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            return numbers.Sum(x => x);
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers?.Where(x => x % 2 == 0).Sum() ?? 0;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+                return false;
+            return numbers.ToArray().Sum() % 2 != 0;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number <= 0)
+            {
+                return 0;
+            }
+            else
+                return number / 2;
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,17 +8,40 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            if(vals.Contains(false))
+            {
+                return true;
+            }
+            else
+                return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            var odds = new List<int>();
+            if(numbers != null)
+            {
+                foreach (var num in numbers)
+                {
+                    if (num % 2 != 0)
+                    {
+                        odds.Add(num);
+                    }
+                }
+            
+            }
+            if (odds.Sum() % 2 != 0)
+            {
+                return true;
+            }
+            else
+                return false;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if(password.Contains('')
         }
 
         public char GetFirstLetterOfString(string val)
@@ -32,7 +56,12 @@ namespace ChallengesWithTestsMark8
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if(dividend == 0 || divisor == 0)
+            {
+                return 0;
+            }
+            else
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
